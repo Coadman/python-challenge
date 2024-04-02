fb36212ba04c421caea3c47ad7ca55117f2c9ac6
Coadman <162220090+Coadman@users.noreply.github.com>
2024-04-02 17:03:26 -0500

    save

--- python-challenge/PyBank/Resources/main.py
+++ python-challenge/PyBank/Resources/main.py
@@ -1,10 +1,26 @@
-import csv 
-filepath = "Starter_Code/PyBank/Resources/budget_data.csv"
+# Tasks:
 
-row_number = 0
+# The total number of months included in the dataset
 
-with open(filepath) as text:
-    csv_file = csv.reader(text)
+# The net total amount of "Profit/Losses" over the entire period
 
-    # for row in csv_file:
-    #     if row_number
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in profits (date and amount) over the entire period
+
+# Start Here:
+
+import csv
+
+#create variables
+budget_file_path = "PyPoll/Resources/budget_data.csv"
+month_and_yr = []
+# open file
+with open(budget_file_path) as budget_file:
+    csv_file = csv.reader(budget_file)
+    next(csv_file)
+    for row in csv_file:
+        month_and_yr = row[0]
+        print(month_and_yr)
